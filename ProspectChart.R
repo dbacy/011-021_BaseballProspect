947f67a7be98555dace7c5a0675be7f3707edbfe
dbacy <69469697+dbacy@users.noreply.github.com>
2021-02-05 08:06:36 -0600

    Update ProspectChart.R
    Next move to add up the # of prospects to then place in LGI on Y

--- ProspectChart.R
+++ ProspectChart.R
@@ -17,7 +17,7 @@ kable(df) %>%
 summary(testData)
 
 aggData <- aggregate(testData, by = list(testData$Year),
-                     FUN = mean, na.rm=FALSE)
+                   FUN = mean, na.rm=FALSE)
 
 aggData_2011 <- filter(testData , Year == "2011")
 aggData_2012 <- filter(testData , Year == "2012")
@@ -29,11 +29,31 @@ aggData_2017 <- filter(testData , Year == "2017")
 aggData_2018 <- filter(testData , Year == "2018")
 aggData_2019 <- filter(testData , Year == "2019")
 aggData_2020 <- filter(testData , Year == "2020")
-aggData_2021 <- filter(testData , Year == "2yy021")
+aggData_2021 <- filter(testData , Year == "2021")
 
-LGI <- ggplot() + geom_line(aes(y = Total, x= Positions),
+LGI <- ggplot() + geom_line(aes(y = Total, x= Position),
                             size= 0.8, color = "blue",
                             data = aggData_2011) +
   geom_line(aes(y= Total , x = Postition), 
             size= 0.8, color = "red", data = aggData_2012) +
+  geom_line(aes(y= Total, x= Position),
+            size= 0.8, color = "purple",data = aggData_2013) +
+  geom_line(aes(y= Total, x= Position),
+            size= 0.8, color = "green",data = aggData_2014) +
+  geom_line(aes(y= Total, x= Position),
+            size= 0.8, color = "orange",data = aggData_2015) +
+  geom_line(aes(y= Total, x= Position),
+            size= 0.8, color = "teal",data = aggData_2016) +
+  geom_line(aes(y= Total, x= Position),
+            size= 0.8, color = "black",data = aggData_2017) +
+  geom_line(aes(y= Total, x= Position),
+            size= 0.8, color = "moroon",data = aggData_2018) +
+  geom_line(aes(y= Total, x= Position),
+            size= 0.8, color = "brown",data = aggData_2019) +
+  geom_line(aes(y= Total, x= Position),
+            size= 0.8, colr = "yellow",data = aggData_2020) +
+  geom_line(aes(y= Total, x= Position),
+            size= 0.8, colr = "blue",data = aggData_2021) 
+  
+  
   
